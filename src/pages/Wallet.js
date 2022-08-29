2b40bba73a07d3e6f6b2bde06a6b1aaf0d3ba70f
ItzMari0 <msajr@outlook.com>
2022-08-29 14:34:54 -0300

    Requisito 2 resolvido

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
+import WalletForm from '../components/WalletForm';
 
-export default class Wallet extends Component {
+class Wallet extends Component {
   render() {
     return (
-      <div>Wallet</div>
+      <>
+        <header>
+          <Header />
+        </header>
+        <main>
+          <WalletForm />
+        </main>
+      </>
     );
   }
 }
+
+export default Wallet;
